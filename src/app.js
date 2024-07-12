f3cb38ffe5dd8b3efc1f709202cabd474979fb7c
Rommel Vargas <romking39@yahoo.es>
2024-07-12 11:27:59 -0600

    Limpiando el app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 import express from "express";
 import cors from "cors";
-import getUsers  from "./routers/usuarios.route.js";
-import getUserById from "./routers/usuarios.route.js";
-import createUser from "./routers/usuarios.route.js";
-import  putUser from "./routers/usuarios.route.js";
-import deleteUser from "./routers/usuarios.route.js";
+import rutaUsuario  from "./routers/usuarios.route.js";
+import rutaClientes from "./routers/cliente.route.js";
 
-//importaciones clientes
-import getClientes from "./routers/cliente.route.js";
-import getClienteById from "./routers/cliente.route.js";
-import createCliente from "./routers/cliente.route.js";
-import updateCliente from "./routers/cliente.route.js";
-import deleteCliente from "./routers/cliente.route.js";
  
 
 const app = express();
@@ -22,19 +13,10 @@ app.use(cors());
 // Middleware para parsear JSON y datos urlencoded
 app.use(express.json());
 
-//Rutas para endpoint usuario.
-app.use("/api", getUsers);
-app.use("/api", getUserById);
-app.use("/api", createUser);
-app.use("/api", putUser);
-app.use("/api", deleteUser);
+//Rutas .
+app.use("/api", rutaUsuario);
+app.use("/api", rutaClientes);
 
-//rutas para endpoint clientes.
-app.use("/api", getClientes);
-app.use("/api", getClienteById);
-app.use("/api", createCliente);
-app.use("/api",updateCliente);
-app.use("/api", deleteCliente);
 
 // Manejar rutas no encontradas
 app.use((req, res, next) => {
